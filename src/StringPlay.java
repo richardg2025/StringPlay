a59b66426982c89dc3c3bc1f6a2aaef53a3aa17f
techarenz <57818506+techarenz@users.noreply.github.com>
2023-01-20 07:46:57 +0800

    Update StringPlay.java

--- src/StringPlay.java
+++ src/StringPlay.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class StringPlay {
+public class StringPlay {
+  
+  public static void main(String[] args) {
+
+        // 1. Create a String called myFavouriteArtist and initialize it to your favourite music artist.
+
+        // 2. Print myFavouriteArtist.
+
+        // 3. Create a variable called myFavouriteArtistLength and initialize it to the length of myFavouriteArtist.
+        //    Ensure to use a method call to achieve this.
+
+        // 4. Print myFavouriteArtistLength.
+
+        // 5. Create a String called myFavouriteArtistUpper that is equal to myFavouriteArtist except every character is uppercase.
+        //    Ensure to use a method call to achieve this.
+
+        // 6. Print myFavouriteArtistUpper.
+
+        // 7. Create a String called myFavouriteArtistLower that is equal to myFavouriteArtist except every character is lowercase.
+        //    Ensure to use a method call to achieve this.
+
+        // 8. Print myFavouriteArtistLower.
+
+        // 9. Create a variable called checkFor that is equal to “ift”.
+
+        // 10. Create a variable called myFavouriteArtistContains that is equal to whether myFavouriteArtist contains checkFor or not.
+        //     Ensure to use a method call to achieve this.
+
+        // 11. Print myFavouriteArtistContains.
+
+        // 12. Create a variable called firstName and initialize it to your first name.
+
+        // 13. Create a variable called lastName and initialize it to your last name.
+
+        // 14. Create a variable called age and initialize it to your age.
+
+        // 15. Declare a variable called myInfo.
+
+        // 16. Initialize myInfo (note that the declaration and initialization are separate statements) to be equal to a String that
+        //     contains a description that includes the variables firstName, lastName, age, and myFavouriteArtist.
+        //     The format of the String should be, for example, as follows:
+
+            /*  First name: Keanu
+                Last name: Reeves
+                Age: 58
+                Favourite artist: Biggie Smalls
+            */
+
+        // 17. Print myInfo.
+
+        // 18. Create a String called stringToIntTest and initialize it to “16”.
+
+        // 19. Create an int called stringToIntTestStore and initialize it to an integer representation of stringToIntTest.
+        //     Ensure to use a method call to achieve this.
+
+        // 20. Create a variable called intToStringTest and initialize it to a String representation of stringToIntTestStore.
+        //     Ensure to use String concatenation to achieve this.
+
+    }
 }
